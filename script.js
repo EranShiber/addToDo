052e51e6adff10be035fe2464e32ebd856812894
Eran Shiber <44651526+Shibex@users.noreply.github.com>
2018-12-09 21:55:11 +0200

    mark todo items

--- script.js
+++ script.js
@@ -3,25 +3,40 @@ const form = document.getElementById("form");
 const ul = document.getElementById("list");
 ul.addEventListener("click", unDo);
 form.addEventListener("submit", addDo);
+ul.addEventListener("click", completeTodo)
+
+
 
 function addDo(e){
     e.preventDefault();
+   
    let li = document.createElement("li");
-   li.className = "list-group-item"
+   li.className = "list-group-item myLi"
    li.innerHTML = input.value;
+   if(input.value === ""){} else {
     let delBtn = document.createElement("button")
-    delBtn.className = "delete btn btn-danger float right"
+    delBtn.className = "delete btn btn-danger"
     delBtn.innerText = "Delete"
     li.appendChild(delBtn)
     ul.appendChild(li); 
     input.value = ""   
+   }
 }
-
 function unDo(e) {
+    if (e.target.className === "delete btn btn-danger"){
     let li = e.target.parentElement;
-    ul.removeChild(li)
-    
+    ul.removeChild(li)    
+    }
 }
 
-        
+function completeTodo(e){
+    let li = e.target;
+    if(li.style.textDecoration === ""){
+    li.style.textDecoration = "line-through"
+    } else if (li.style.textDecoration = ""){
+        li.style.textDecoration = "line-through"
+    }
+     
+}
+
     
